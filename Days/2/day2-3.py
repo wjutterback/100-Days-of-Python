89d722372d26a10cc1134fb0d69f7ac4ac29837e
Will Utterback <uwill08@gmail.com>
2022-01-12 03:30:02 -0600

    converted to snake_case

--- Days/2/day2-3.py
+++ Days/2/day2-3.py
@@ -6,8 +6,8 @@ age = input("What is your current age?")
 inputYears = int(age)
 years_remaining = 90 - inputYears
 
-calculatedDay = years_remaining * 365
-calculatedWeeks = years_remaining * 52
-calculatedMonths = years_remaining * 12
+calculated_day = years_remaining * 365
+calculated_weeks = years_remaining * 52
+calculated_months = years_remaining * 12
 
-print(f"You have {calculatedDay} days, {calculatedWeeks} weeks, and {calculatedMonths} months left.")
+print(f"You have {calculated_day} days, {calculated_weeks} weeks, and {calculated_months} months left.")
