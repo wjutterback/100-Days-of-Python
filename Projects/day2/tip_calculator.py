231f9a1e32de03648e313d5860e194da92585487
Will Utterback <uwill08@gmail.com>
2022-01-12 03:47:58 -0600

    implemented rounding

--- Projects/day2/tip_calculator.py
+++ Projects/day2/tip_calculator.py
@@ -3,6 +3,5 @@ bill_total = float(input('What was the total bill? $'))
 percentage_tip = input('What percentage tip would you like to give? 10, 12, or 15?')
 bill_split = input('How many people to split the bill?')
 
-
-bill = int(bill_total) * (1 + int(percentage_tip)/100) / int(bill_split)
+bill = round(bill_total * (1 + int(percentage_tip)/100) / int(bill_split), 2)
 print(f'Each person should pay: {bill}')
