ed083811bbae71c769da69f284b4a703396c45c7
Will Utterback <uwill08@gmail.com>
2022-01-12 02:46:28 -0600

    Day2-1 finished

---
+++ Days/2/day2-1.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+result = 0
+for i in two_digit_number:
+  result += int(i)
+
+print(result)
