ed9b2e856d34e94b797825f01703f4965d90ae6d
Will Utterback <uwill08@gmail.com>
2022-01-12 03:51:49 -0600

    Implemented string format

--- Projects/day2/tip_calculator.py
+++ Projects/day2/tip_calculator.py
@@ -3,5 +3,6 @@ bill_total = float(input('What was the total bill? $'))
 percentage_tip = input('What percentage tip would you like to give? 10, 12, or 15?')
 bill_split = input('How many people to split the bill?')
 
-bill = round(bill_total * (1 + int(percentage_tip)/100) / int(bill_split), 2)
-print(f'Each person should pay: {bill}')
+bill = bill_total * (1 + int(percentage_tip)/100) / int(bill_split)
+formatted_bill = "{:.2f}".format(bill)
+print(f'Each person should pay: ${formatted_bill}')
